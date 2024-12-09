6c2735b6c6319b6c2e4d3f220fb79d9447ee4007
acidmath <adrian.ajx@gmail.com>
2024-12-09 17:51:23 -0600

    about to make a list of triangles

--- DinnerSpinner/UI/Element/SpinnerElement.xaml.cs
+++ DinnerSpinner/UI/Element/SpinnerElement.xaml.cs
@@ -1,15 +1,31 @@
 ﻿using DinnerSpinner.UI.ViewModel;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace DinnerSpinner.UI.Element {
     
     public partial class SpinnerElement : UserControl
     {
+        public static readonly DependencyProperty CanvasWidthProperty =
+            DependencyProperty.Register("CanvasWidth", typeof(double), typeof(SpinnerElement), new PropertyMetadata(default(double)));
+
+        public double CanvasWidth {
+            get { return (double)GetValue(CanvasWidthProperty); }
+            set { SetValue(CanvasWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty CanvasHeightProperty =
+            DependencyProperty.Register("CanvasHeight", typeof(double), typeof(SpinnerElement), new PropertyMetadata(default(double)));
+
+        public double CanvasHeight {
+            get { return (double)GetValue(CanvasHeightProperty); }
+            set { SetValue(CanvasHeightProperty, value); }
+        }
+
         public SpinnerElement()
-        {
-            DataContext = new SpinnerViewModel();
+        {            
             InitializeComponent();
-            
+            DataContext = this;
         }
     }
 }
--- DinnerSpinner/UI/ViewModel/SpinnerViewModel.cs
+++ DinnerSpinner/UI/ViewModel/SpinnerViewModel.cs
@@ -3,25 +3,11 @@
 namespace DinnerSpinner.UI.ViewModel {
     public class SpinnerViewModel : DependencyObject {
 
-        public static readonly DependencyProperty WidthProperty =
-            DependencyProperty.Register("Width", typeof(double), typeof(SpinnerViewModel), new PropertyMetadata(default(double)));
 
-        public double Width {
-            get { return (double)GetValue(WidthProperty); }
-            set { SetValue(WidthProperty, value); }
-        }
 
-        public static readonly DependencyProperty HeightProperty =
-            DependencyProperty.Register("Height", typeof(double), typeof(SpinnerViewModel), new PropertyMetadata(default(double)));
+        public SpinnerViewModel(double canvasHeight, double canvasWidth) {
 
-        public double Height {
-            get { return (double)GetValue(HeightProperty); }
-            set { SetValue(HeightProperty, value); }
-        }
-
-        public SpinnerViewModel() {
-
-            var balls = (Height, Width);
+           // var balls = (CanvasHeight, CanvasWidth);
             ;
         }
 
